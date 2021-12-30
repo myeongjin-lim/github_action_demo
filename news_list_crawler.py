5442cf00fb560175fbbb3ca262d1dfd26427933a
임명진 <mjl7173@naver.com>
2021-12-30 13:47:33 +0900

    Update news_list_crawler.py

--- news_list_crawler.py
+++ news_list_crawler.py
@@ -41,9 +41,9 @@ cursor = conn.cursor()
 
 sql = 'insert into news_link values(:1,:2)'
 
-# for href in news_href_list:
-#     cursor.execute(sql,href)
-cursor.execute(sql, news_href_list[0])
+for href in news_href_list:
+    cursor.execute(sql,href)
+# cursor.execute(sql, news_href_list[0])
     
 print('저장된 링크수>>',cursor.rowcount)
 
