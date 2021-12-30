cbb1076a71480cbb98e3c413a22a6173baa5b246
임명진 <mjl7173@naver.com>
2021-12-30 10:29:35 +0900

    Update news_list_crawler.py
    함수형태 제거

--- news_list_crawler.py
+++ news_list_crawler.py
@@ -4,14 +4,16 @@ from os.path import abspath
 from bs4 import BeautifulSoup as bs
 
 BASE_DIR = os.path.dirname(os.path.abspath("__file__"))
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
 
-def crawling_hankyung_news_link(news_url, pageNum):
-    news_href_list = []
+category = ['0401','0402','0403','0404','0405','0408','0409']
+news_href_list = []
+
+for cate in category:
     
-    for pageNo in range(1,pageNum+1):
-        url = '{}{}'.format(news_url, pageNo)
-        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
-
+    for pageNo in range(1,11):
+        url = 'https://www.hankyung.com/economy/{}?page={}'.format(cate, pageNo)
+        
         res = req.get(url, headers = headers)
         html = bs(res.content, 'lxml')
 
@@ -19,20 +21,9 @@ def crawling_hankyung_news_link(news_url, pageNum):
 
         for tit in tit_tags:
             news_href_list.append(tit.get('href'))  
-            
-    return news_href_list
 
-def save_file(data):
-    file = open(os.path.join(BASE_DIR, 'news_list.txt'), 'w', encoding='utf-8')
-    file.write(data)
-    file.close()
-    print('저장완료>>',os.path.join(BASE_DIR, 'news_list.txt'))
+file = open(os.path.join(BASE_DIR, 'news_list.txt'), 'w', encoding='utf-8')
+file.write(str(news_href_list)
+file.close()
 
-category = ['0401','0402','0403','0404','0405','0408','0409']
-news_href_list = []
-
-for cate in category:
-    url = 'https://www.hankyung.com/economy/{}?page='.format(cate)
-    news_href_list.append(crawling_hankyung_news_link(url, 10))
-    
-save_file(str(news_href_list))
+print('수집완료>>',os.path.join(BASE_DIR, 'news_list.txt'))
